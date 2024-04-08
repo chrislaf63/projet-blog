2f1d9fb9244c2b2b84695eaf570c41d9154ad734
Christophe LAFARGE <lafarge.christophe@etik.com>
2024-04-08 11:03:16 +0200

    premier commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// import builder where defaultStringLength method is defined
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Fix for MySQL < 5.7.7 and MariaDB < 10.2.2
+        Schema::defaultStringLength(191); //Update defaultStringLength
     }
+
 }
